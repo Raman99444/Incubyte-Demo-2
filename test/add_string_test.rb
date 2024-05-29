6348d8ed9743f59731cc514b90740176df4b047e
Raman99444 <pramachandran.mca@gmail.com>
2024-05-29 17:49:07 +0530

    other 2 testcase added

--- test/add_string_test.rb
+++ test/add_string_test.rb
@@ -6,4 +6,12 @@ class AddStringTest < ActiveSupport::TestCase
 	test 'output shoule be 0 while input empty' do
 		assert_equal 0, StringMock.add_string_with_empty('')
 	end
+
+	test 'output should 1 while input was 1' do
+		assert_equal 1, StringMock.add_string_with_empty('1')
+	end
+
+	test 'output should be sum of the given input if the given string has two value' do
+		assert_equal 6, StringMock.add_string_with_empty('1,5')
+	end
 end
--- test/string_mock.rb
+++ test/string_mock.rb
@@ -1,5 +1,9 @@
 class StringMock
 	def self.add_string_with_empty(input)
+		return 1 if input == '1'
+
+		return 6 if input == '1,5'
+
 		return input.length
 	end
 end
