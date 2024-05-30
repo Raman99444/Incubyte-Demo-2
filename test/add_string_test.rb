a5a7f57b66fe5e20bea97584bffb6503ea6f255d
Raman99444 <pramachandran.mca@gmail.com>
2024-05-30 10:02:08 +0530

    backup and corrected code for personal reference

--- test/add_string_test.rb
+++ test/add_string_test.rb
@@ -2,28 +2,103 @@ require 'test_helper'
 require 'string_mock'
 
 class AddStringTest < ActiveSupport::TestCase
+  setup do
+    @mock = StringMock.new
+  end
 
-	test 'output shoule be 0 while input empty' do
-		assert_equal 0, StringMock.add('')
-	end
+  class EmptyOrNilInputTest < AddStringTest
+    test 'should be 0 while input empty' do
+      assert_equal 0, @mock.add('')
+    end
 
-	test 'output should 1 while input was 1' do
-		assert_equal 1, StringMock.add('1')
-	end
+    test 'should be 0 while input was nil' do
+      assert_equal 0, @mock.add(nil)
+    end
+  end
 
-	test 'output should be sum of the given input if the given string has two value' do
-		assert_equal 6, StringMock.add('1,5')
-	end
+  class InvalidCharactersTest < AddStringTest
+    test 'should be 0 with invalid chars' do
+      assert_equal 0, @mock.add(';')
+    end
 
-	test 'should throw the error if the given number was negative' do
-		assert_equal "negative numbers not allowed -10", StringMock.add('-10')
-	end
+    test 'should return 0 with invalid char sequence' do
+      assert_equal 0, @mock.add('a,b,c,d')
+    end
 
-	test 'should return the sum of the number even the given string contain new line char' do
-		assert_equal 6, StringMock.add('1\n2,3')
-	end
+    test 'should throw an error if the given string was invalid' do
+      error = assert_raises(ArgumentError) do
+        @mock.add("1,\n")
+      end
 
-	test 'should return the correct value with ;' do
-		assert_equal 3, StringMock.add('//;\n1;2')
-	end
+      assert_equal "Invalid input string format", error.message
+    end
+
+    test 'should throw an error if the given string end with comma' do
+      error = assert_raises(ArgumentError) do
+        @mock.add("1,2,3,")
+      end
+
+      assert_equal "Invalid input string format", error.message
+    end
+  end
+
+  class ValidNumbersTest < AddStringTest
+    test 'output should be 1 while input was 1' do
+      assert_equal 1, @mock.add('1')
+    end
+
+    test 'output should be sum of the given input if the given string has two values' do
+      assert_equal 6, @mock.add('1,5')
+    end
+
+    test 'should return the sum of the number even if the given string contains newline character' do
+      assert_equal 6, @mock.add("1\n2,3")
+    end
+
+    test 'should not include numbers greater than 1000 in the sum' do
+      assert_equal 6, @mock.add('1,5,1001,2000')
+    end
+
+    test 'should include the number 1000 in the sum' do
+      assert_equal 1006, @mock.add('1,5,1000,2000')
+    end
+  end
+
+  class CustomDelimitersTest < AddStringTest
+    test 'should return the correct value with ;' do
+      assert_equal 3, @mock.add("//;\n1;2")
+    end
+
+    test 'should return the correct value with custom delimiter' do
+      assert_equal 6, @mock.add("//***\n1***2***3")
+    end
+
+    test 'should return the correct value with delimiter within []' do
+      assert_equal 6, @mock.add("//[***]\n1***2***3")
+    end
+
+    test 'should return the correct value with multiple delimiters' do
+      assert_equal 6, @mock.add("//[*][%]\n1*2%3")
+    end
+  end
+
+  class NegativeNumbersTest < AddStringTest
+    test 'should throw an error if the given number is negative' do
+      error = assert_raises(ArgumentError) do
+        @mock.add('-10')
+      end
+
+      assert_equal "negative numbers not allowed -10", error.message
+    end
+
+    test 'should return error with all the given negative numbers' do
+      error = assert_raises(ArgumentError) do
+        @mock.add('-10,90,-20,70,-30')
+      end
+
+      assert_equal "negative numbers not allowed -10,-20,-30", error.message
+    end
+  end
 end
+
+
--- test/string_mock.rb
+++ test/string_mock.rb
@@ -1,21 +1,46 @@
 class StringMock
-	def self.add(input)
-		return 0 if input.empty?
+	def add(input)
+		return 0 if input.nil? || input.empty?
 
-		negative = input[0] == '-'
-		return "negative numbers not allowed #{input}" if negative
-
-		# given input was single?, then convert and return
-		return input.to_i if input.length == 1
+	    raise ArgumentError, "Invalid input string format" if input.end_with?(',', "\n")
 
 		#Replace the common delemeter as
-		total = 0
-		input.each_char do |chr|
-			next if chr == /\d/
+		str_arr = convert_string_to_array(input)
 
-			total += chr.to_i
+		is_negative = str_arr.select { |val| val < 0 }
+		if is_negative.any?
+			raise ArgumentError, "negative numbers not allowed #{is_negative.join(',')}"
 		end
 
-		return total
+		str_arr.reject! { |val| val > 1000 }
+
+		return str_arr.inject(:+)
 	end
+
+	private
+
+	def convert_string_to_array(s)
+		return [] if s.nil? || s.empty?
+
+	  if s.start_with?("//")
+	    delimiter, s = s[2..-1].split("\n", 2)
+	    delimiter = if delimiter.start_with?('[') && delimiter.end_with?(']')
+                      #Regexp.escape(delimiter[1..-2])
+                      parse_multiple_delimiters(delimiter)
+                    else
+                      Regexp.escape(delimiter)
+                    end
+	  else
+	    delimiter = "\n|,"
+	  end
+
+	  result = s.split(Regexp.new(delimiter))
+
+	  result.map(&:to_i)
+	end
+
+	def parse_multiple_delimiters(delimiter_part)
+	    delimiters = delimiter_part.scan(/\[([^\]]+)\]/).flatten
+	    delimiters.map { |delim| Regexp.escape(delim) }.join('|')
+	  end
 end
